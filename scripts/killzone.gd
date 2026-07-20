a8de5f7205e724efab1fed6c4ad4356301afc3e9
Ahmed18720 <ahmedelbahraey0@gmail.com>
2026-07-21 02:00:31 +0300

    upgrade main menu

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,7 +3,7 @@ extends Area2D
 @onready var timer: Timer = $Timer
 
 func _on_body_entered(body: Node2D) -> void:
-	print("you died!")
+	get_tree().change_scene_to_file("res://sences/lose.tscn")
 	Engine.time_scale=0.5
 	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
