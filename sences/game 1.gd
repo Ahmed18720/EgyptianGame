f770b386aebcf7818f65f4dd140d86bc7b569cc6
Ahmed18720 <ahmedelbahraey0@gmail.com>
2026-07-22 01:02:57 +0300

    make a win design

---
+++ sences/game 1.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+
+var score=0
+
+@onready var score_label: Label = $"score label"
+
+
+func add_point():
+	score +=1
+	score_label.text ="you collected " + str(score)  + " coins."
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print(11111)
+	if body.name == "player":
+		get_tree().change_scene_to_file("res://sences/level_2.tscn")
