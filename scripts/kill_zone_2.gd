c669ca06877aeddbf1de3619a0005cba27f639ec
Ahmed18720 <ahmedelbahraey0@gmail.com>
2026-08-03 02:57:11 +0300

    make kill menu to level 2

--- scripts/kill_zone_2.gd
+++ scripts/kill_zone_2.gd
@@ -2,4 +2,4 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://sences/level_2.tscn")
+	get_tree().change_scene_to_file("res://sences/kill_menu_2.tscn")
