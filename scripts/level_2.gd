9ed966b78a1d336e971bf3ec129b7914dd183cd9
Ahmed18720 <ahmedelbahraey0@gmail.com>
2026-08-05 22:02:25 +0300

    make level 3

--- scripts/level_2.gd
+++ scripts/level_2.gd
@@ -9,3 +9,8 @@ var score=0
 func add_point():
 	score +=1
 	score_label.text ="you collected " + str(score)  + " coins."
+
+
+func _on_win_zone_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		get_tree().change_scene_to_file("res://sences/level_3.tscn")
